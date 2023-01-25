f5020d14699a39f21d974d011935caf1324f210f
mohamedleithy <leithy@aucegypt.edu>
2023-01-25 13:14:40 +0200

    updating brakingSys.py

--- FMU/brakingSys.py
+++ FMU/brakingSys.py
@@ -19,22 +19,23 @@ def fmu(path,force):
     dtype = [('expseu_.Force', np.float)]
     signals = np.array([Force], dtype=dtype)
     result = simulate_fmu(fmu, stop_time=0.5, input=signals)
+    print(result)
     return result
     
-def pushing_into_file(results):
-    for result in results:
-        front_left = open("front_left.txt", 'a+')
-        front_left.write(f"{result[0]},{result[1]} \n")
-        front_left.close()
-        front_right = open("front_right.txt", 'a+')
-        front_right.write(f"{result[0]},{result[2]} \n")
-        front_right.close()
-        rear_left = open("rear_left.txt", 'a+')
-        rear_left.write(f"{result[0]},{result[3]} \n")
-        rear_left.close()
-        rear_right = open("rear_right.txt", 'a+')
-        rear_right.write(f"{result[0]},{result[4]} \n")
-        rear_right.close()
+# def pushing_into_file(results):
+#     for result in results:
+#         front_left = open("front_left.txt", 'a+')
+#         front_left.write(f"{result[0]},{result[1]} \n")
+#         front_left.close()
+#         front_right = open("front_right.txt", 'a+')
+#         front_right.write(f"{result[0]},{result[2]} \n")
+#         front_right.close()
+#         rear_left = open("rear_left.txt", 'a+')
+#         rear_left.write(f"{result[0]},{result[3]} \n")
+#         rear_left.close()
+#         rear_right = open("rear_right.txt", 'a+')
+#         rear_right.write(f"{result[0]},{result[4]} \n")
+#         rear_right.close()
 
 def plotting(axis,lines):
     xs = []
@@ -56,8 +57,9 @@ def animate(i,axis,path):
     lines = graph_data.split('\n')
     plotting(axis,lines)
 
-pushing_into_file(fmu("/home/g02-f22/Downloads/ToSend/FMI/BrakingSystem.fmu",50))
-ani_1 = animation.FuncAnimation(fig,animate(ax1,"front_left_path"), interval=1000)
-ani_2 = animation.FuncAnimation(fig,animate(ax1,"front_right_path"), interval=1000)
-ani_3 = animation.FuncAnimation(fig,animate(ax1,"rear_left_path"), interval=1000)
-ani_4 = animation.FuncAnimation(fig,animate(ax1,"rear_right_path"), interval=1000)
+fmu("/home/g02-f22/Downloads/ToSend/FMI/BrakingSystem.fmu",50)
+#pushing_into_file(fmu("/home/g02-f22/Downloads/ToSend/FMI/BrakingSystem.fmu",50))
+# ani_1 = animation.FuncAnimation(fig,animate(ax1,"front_left_path"), interval=1000)
+# ani_2 = animation.FuncAnimation(fig,animate(ax1,"front_right_path"), interval=1000)
+# ani_3 = animation.FuncAnimation(fig,animate(ax1,"rear_left_path"), interval=1000)
+# ani_4 = animation.FuncAnimation(fig,animate(ax1,"rear_right_path"), interval=1000)
