68ee78762712528929fea39466a482288995292c
mohamedleithy <leithy@aucegypt.edu>
2023-01-25 10:26:30 +0200

    adding HOST_IP env variable

--- turtlebot3_docker/run_simulation.bash
+++ turtlebot3_docker/run_simulation.bash
@@ -5,7 +5,7 @@ docker run -d --net=ros -p 11311:11311 --name roscore osrf/ros:noetic-desktop-fu
 
 
 
-docker run -it --net=ros --env="DISPLAY=novnc:0.0" --env="ROS_MASTER_URI=http://roscore:11311" --env="TURTLEBOT3_MODEL=waffle_pi" turtlebot3_sim bash
+docker run -it --net=ros --env="DISPLAY=novnc:0.0" --env="HOST_IP=10.40.7.108" --env="ROS_MASTER_URI=http://roscore:11311" --env="TURTLEBOT3_MODEL=waffle_pi" turtlebot3_sim bash
 source ~/catkin_ws/devel/setup.sh
 
 roslaunch turtlebot3_gazebo turtlebot3_house.launch
