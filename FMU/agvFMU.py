b85cf4548a1c2c9bf889219d1589136a40e353e2
mohamedleithy <leithy@aucegypt.edu>
2023-02-12 16:07:11 +0200

    relative path

--- FMU/agvFMU.py
+++ FMU/agvFMU.py
@@ -55,7 +55,7 @@ def pushing_into_file(results):
 
 
 
-fmu("/home/g02-f22/Downloads/source_code/AUC-Thesis-DT/FMU/AGV_turtle_electric_drive_Prescan_export.fmu",2.6, 0.1, 0.1)
-pushing_into_file(fmu("/home/g02-f22/Downloads/source_code/AUC-Thesis-DT/FMU/AGV_turtle_electric_drive_Prescan_export.fmu",2.6, 0.1, 0.1))
+fmu("./AGV_turtle_electric_drive_Prescan_export.fmu",2.6, 0.1, 0.1)
+pushing_into_file(fmu("./AGV_turtle_electric_drive_Prescan_export.fmu",2.6, 0.1, 0.1))
 
 
