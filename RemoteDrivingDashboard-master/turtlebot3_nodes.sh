1dec864890ff152f5758b46b790df8d3071935bf
mohamedleithy <leithy@aucegypt.edu>
2023-02-14 11:55:35 +0200

    bash for running lower nodes

---
+++ RemoteDrivingDashboard-master/turtlebot3_nodes.sh
@@ -0,0 +1,3 @@
+nohup python3 ~/catkin_ws/src/actuate/src/actuate.py > output1.log &
+nohup python3 ~/catkin_ws/src/laser_values/src/scan.py  > output1.log &
+nohup python3 ~/catkin_ws/src/sensor_fusion/src/sensor_fusion.py  > output1.log &
